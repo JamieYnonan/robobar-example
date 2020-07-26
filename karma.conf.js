a5b4357af5c0226b15d2f3dcc1dccc442f750438
Jamie Ynonan <jamiea31@gmail.com>
2020-07-26 12:08:18 -0500

    add debug port chrome

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,18 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
     singleRun: true,
     concurrency: Infinity
   });
